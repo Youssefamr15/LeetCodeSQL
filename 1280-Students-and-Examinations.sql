2f6c3e307c5bb87bdec57a09dc8a5c1b81f5d893
Youssef Amr <88115521+Youssefamr15@users.noreply.github.com>
2024-09-05 20:00:00 +0300

    [1280] [Time Beats: 95.19%] - LeetPush

--- 1280-Students-and-Examinations.sql
+++ 1280-Students-and-Examinations.sql
@@ -1,12 +1,9 @@
-SELECT 
-    s.student_id,s.student_name ,su.subject_name,COUNT(e.student_id) AS 'attended_exams'
-FROM 
-    Students s
-CROSS JOIN
-    Subjects su
-LEFT JOIN 
-    Examinations e ON s.student_id = e.student_id AND su.subject_name = e.subject_name
-GROUP BY 
-    1,2,3
-ORDER BY 
-    s.student_id,su.subject_name
+SELECT s.student_id,student_name ,su.subject_name,COUNT(e.student_id) AS 'attended_exams'
+FROM Students s
+CROSS JOIN Subjects su
+LEFT JOIN Examinations e 
+ON s.student_id = e.student_id 
+AND su.subject_name = e.subject_name
+GROUP BY 1,2,3
+ORDER BY s.student_id,su.subject_name
+;
