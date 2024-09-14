ab78e1a3fdab25c7a05faf81afcf71050e88b55e
Youssef Amr <88115521+Youssefamr15@users.noreply.github.com>
2024-09-14 21:06:37 +0300

    [550] [Time Beats: 96.22%] - LeetPush

---
+++ 550-Game-Play-Analysis-IV.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+WITH temp AS (
+    SELECT player_id, MIN(event_date) AS first_login_date
+    FROM Activity 
+    GROUP BY player_id
+)
+
+# Calculate the fraction of players who logged in exactly one day after their first login.
+SELECT 
+    ROUND(
+        SUM(DATEDIFF(a.event_date, t.first_login_date) = 1) / COUNT(DISTINCT a.player_id), 2
+    ) AS fraction
+FROM Activity a
+JOIN temp t ON a.player_id = t.player_id;
