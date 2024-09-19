879f42b9284538f450962804b343194136900bde
Youssef Amr <88115521+Youssefamr15@users.noreply.github.com>
2024-09-20 02:15:35 +0300

    [619] [Time Beats: 81.01%] - LeetPush

--- 619-Biggest-Single-Number.sql
+++ 619-Biggest-Single-Number.sql
@@ -1,11 +1,12 @@
 # Write your MySQL query statement below
 SELECT
    MAX(num) AS num
-FROM(
-SELECT num
-FROM MyNumbers
-GROUP BY num 
-HAVING COUNT( num ) = 1
-ORDER BY num DESC
-LIMIT 1
-) AS uniqueNums
+FROM
+    (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+    ORDER BY num DESC
+    ) AS uniqueNums
+;
